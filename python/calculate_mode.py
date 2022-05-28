a31cf8e6f56a4a88ff5fca006f1a6e36d1e3eed9
Justin <jjpeterson90@hotmail.com>
2022-05-28 13:39:29 -0500

    complete

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,18 @@
-def calculate_mode():
-    pass
+def calculate_mode(input):
+    cache = {}
+    final_list = [];
+    for item in input:
+        if item not in cache:
+            cache[item] = 1
+        else:
+            cache[item] += 1
+    
+    sorted_cache = dict(sorted(cache.items(), key=lambda x: x[1], reverse = True))
+    
+    for key in sorted_cache:
+        if len(final_list) == 0:
+            final_list.append(key)
+        elif sorted_cache.get(key) == list(sorted_cache.values())[0]:
+            final_list.append(key)
+
+    return final_list
