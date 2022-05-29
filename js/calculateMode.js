7b37cd30943f535602cecc8685ef928e0b7c1d42
Justin <jjpeterson90@hotmail.com>
2022-05-29 12:51:27 -0500

    complete

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,22 @@
-const calculateMode = () => {
-
+exports.calculateMode = function(arr) {
+    let countObj = {};
+    let modeList = [];
+    arr.forEach(elem => {
+        if (elem in countObj === false) {
+            countObj[elem] = 1;
+        } else {
+            countObj[elem] += 1;
+        }
+    })
+    Object.keys(countObj).forEach(key => {
+        if (modeList.length === 0) {
+            modeList.push(key);
+        } else if (countObj[key] === countObj[modeList[0]]) {
+            modeList.push(key);
+        } else if (countObj[key] > countObj[modeList[0]]) {
+            modeList = [];
+            modeList.push(key);
+        }
+    })
+    return modeList
 }
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,8 @@
-// Write your own unit tests here!
+let cm = require('./calculateMode');
+
+console.log(cm.calculateMode([1,2,3,3]))            // => [3]
+console.log(cm.calculateMode([4.5, 0, 0]))         // => [0]
+console.log(cm.calculateMode([1.5, -1, 1, 1.5]))    // => [1.5]
+console.log(cm.calculateMode([1,1,2,2]))            // => [1,2]
+console.log(cm.calculateMode([1,2,3]))              // => [1,2,3], because all occur with equal frequency
+console.log(cm.calculateMode(["who", "what", "where", "who"]))  // => ["who"]
